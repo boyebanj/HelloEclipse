aaea31a26ba65186337c4576fdd583deb295b5dd
boyebanj <boyebanj@id414m15.cs.unb.ca>
2016-10-20 13:48:15 -0300

    commited by bridget

---
+++ workspace/Lab5Bridget/src/InfoPrintOyebanji.java
@@ -0,0 +1,11 @@
+
+public class InfoPrintOyebanji 
+{
+
+	
+	public void printOyebanji()
+	{
+		
+		System.out.println("Bridget Oyebanji");
+	}
+}
---
+++ workspace/Lab5Bridget/src/infoAppOyebanji.java
@@ -0,0 +1,14 @@
+
+public class infoAppOyebanji 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Team 13 member list");
+		
+		InfoPrintOyebanji bridget = new InfoPrintOyebanji();
+		bridget.printOyebanji();
+	}
+
+}
